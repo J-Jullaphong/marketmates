b4883231bc47e6d98ea6b9c890305124e9579728
Pichayanon Toochinda <kewkunggt@gmail.com>
2025-02-07 22:45:28 +0700

    Implement base of forum detail view

--- marketmates/forms/__init__.py
+++ marketmates/forms/__init__.py
@@ -1,2 +1,3 @@
 from .expert_registration_form import ExpertRegistrationForm
 from .user_registration_form import UserRegistrationForm
+from .comment_form import CommentForm
--- marketmates/views/forum_detail_view.py
+++ marketmates/views/forum_detail_view.py
@@ -1,8 +1,30 @@
 from django.views.generic import DetailView
-from ..models import Forum
+from django.shortcuts import redirect
+from ..models import Forum, Comment, Tag
+from ..forms import CommentForm
 
 
 class ForumDetailView(DetailView):
     model = Forum
     template_name = "marketmates/forum_detail.html"
     context_object_name = "forum"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["comments"] = Comment.objects.filter(forum=self.object).order_by("-created_at")
+        context["tags"] = Tag.objects.all()[:6]
+        context["form"] = CommentForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.forum = self.object
+            comment.user = request.user
+            comment.save()
+            return redirect("marketmates:forum_detail", pk=self.object.pk)
+
+        return self.get(request, *args, **kwargs)
