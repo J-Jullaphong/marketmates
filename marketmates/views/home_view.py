42bef586b2f3807e1d8e72c850c98672da7741d4
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2025-02-12 21:13:52 +0700

    Add data fetching if not available to HomeView

--- marketmates/views/home_view.py
+++ marketmates/views/home_view.py
@@ -1,8 +1,9 @@
-from django.views.generic import TemplateView
-from django.db.models import Count
 from django.core.cache import cache
+from django.db.models import Count
+from django.views.generic import TemplateView
 
 from ..models import Forum, Tag, ChatRoom
+from ..tasks import fetch_and_store_market_data
 
 
 class HomeView(TemplateView):
@@ -14,9 +15,13 @@ class HomeView(TemplateView):
         context["tags"] = Tag.objects.annotate(
             forum_count=Count("forum")
         ).order_by("-forum_count")[:5]
-        context["private_groups"] = ChatRoom.objects.filter(is_public=False)[:5]
+        context["private_groups"] = ChatRoom.objects.filter(is_public=False)[
+                                    :5]
 
         market_data = cache.get('market_data')
+        if not market_data:
+            market_data = fetch_and_store_market_data.delay().get()
+
         context.update(market_data)
 
         return context
