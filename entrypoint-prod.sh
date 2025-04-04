a2eec50a87fc19dd4a39a47b302118c91f6ea314
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2025-04-04 21:45:43 +0700

    Update entrypoints

--- entrypoint-prod.sh
+++ entrypoint-prod.sh
@@ -8,6 +8,9 @@ python -m venv venv
 # Activate virtual environment
 source venv/bin/activate
 
+# Install dependencies
+pip install -r requirements.txt
+
 # Apply database migrations
 python manage.py migrate
 
